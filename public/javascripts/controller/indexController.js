8e5c028bfaee65b7b657c38c63fafb734d505ce8
emre-unaldi <emree.unaldi@gmail.com>
2021-12-24 15:22:31 +0300

    newUser Login

--- public/javascripts/controller/indexController.js
+++ public/javascripts/controller/indexController.js
@@ -1,13 +1,26 @@
 app.controller('indexController', ['$scope', 'indexFactory', ($scope, indexFactory) => {
-    const connectionoptions = {
-        reconnectionAttempts: 3,
-        reconnectionDelay: 600
+    
+    $scope.init = () => {
+        const username = prompt('Please enter username');
+        if (username) {
+            initSocket(username);
+        } else {
+            return false;
+        }
     };
     
-    indexFactory.connectSocket('http://localhost:3000', connectionoptions)
-      .then((socket) => {
-          console.log('Bağlantı gerçekleşti', socket);
-      }).catch((err) => {
-          console.log(err);
-      });
+    function initSocket(username) {
+        const connectionoptions = {
+            reconnectionAttempts: 3,
+            reconnectionDelay: 600
+        };
+        
+        indexFactory.connectSocket('http://localhost:3000', connectionoptions)
+          .then((socket) => {
+            socket.emit('newUser', { username });
+          }).catch((err) => {
+            console.log(err);
+          });
+    };
+    
 }]);
--- src/socketApi.js
+++ src/socketApi.js
@@ -1,11 +1,27 @@
+const objectAssign = require('object-assign');
 const socketio = require('socket.io');
 const io = socketio();
-const socketApi = {};
 
+const socketApi = {};
 socketApi.io = io;
+
+const users = [];
 
 io.on('connection', (socket) => {
     console.log('a user connected');
+
+    socket.on('newUser', (data) => {
+        const defaultData = {
+            id: socket.id,
+            position:  {
+                x: 0,
+                y: 0
+            }
+        }
+
+        const userData = objectAssign(data, defaultData); // iki değeri birleştirerek tek nesne olarak bize döndürür.
+        users.push(userData); // datayı users dizimize pushluyoruz.
+    });
 });
 
 module.exports = socketApi;
